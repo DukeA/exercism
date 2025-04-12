0e143cba0ae29c81337ebc8dac4782421beb66e2
Adam Grandén <Adam.granden@hotmail.com>
2025-04-12 11:20:14 +0200

    Added Assembly Line , and Log Levels

---
+++ ConsoleApplication/SolutionFolder/LogLevels.cs
@@ -0,0 +1,13 @@
+﻿namespace SolutionFolder;
+
+internal class LogLevels
+{
+    public static string Message(string logLine) =>
+         logLine.Split(new[] { ": " }, 2, StringSplitOptions.None)[1].Trim();
+
+    public static string LogLevel(string logLine) =>
+        logLine.Split(new[] { ": " }, 2, StringSplitOptions.None)[0].Trim('[', ']').ToLower();
+
+    public static string Reformat(string logLine) =>
+        $"{Message(logLine)} ({LogLevel(logLine)})";
+}
---
+++ ConsoleApplication/SolutionFolder/RemoteControlCar.cs
@@ -0,0 +1,21 @@
+﻿namespace SolutionFolder;
+
+internal class RemoteControlCar
+{
+
+    private double _distanceDriven;
+
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+
+    public string DistanceDisplay() => $"Driven {_distanceDriven} meters";
+
+    public string BatteryDisplay()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+    }
+
+    public void Drive()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+    }
+}
